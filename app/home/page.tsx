d09adee564c99223ca488c6ee49670e5bfb1bd65
amelia-liston <aliston@bu.edu>
2025-04-03 19:13:04 -0400

    Adding files

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return(
+        <>
+            <div className="flex flex-col justify-center items-center">
+                <h1>Home page</h1>
+            </div>
+        </>
+    );
+}
