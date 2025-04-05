381681edd37ecd8cca6d3c22401f26e53e23ebeb
amelia-liston <aliston@bu.edu>
2025-04-04 20:22:39 -0400

    Still debugging, added method in fetch

--- app/lib/route.ts
+++ app/lib/route.ts
@@ -12,7 +12,9 @@ export async function getCatData(): Promise<CatTypes[]> {
     // }
     //https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${process.env.CAT_API_KEY}
     //const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10?api_key=${process.env.CAT_API_KEY}`);
-    const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${process.env.CAT_API_KEY}`);
+    const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${process.env.CAT_API_KEY}`, {
+        method: "GET",
+    });
     if(res.status !== 200){
         return [];
     } else {
