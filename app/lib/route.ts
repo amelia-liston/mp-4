e0cf3eeca2e75d0eae39229ccf3bb48c2e0c7bb4
amelia-liston <aliston@bu.edu>
2025-04-04 20:00:42 -0400

    Still debugging deployment issues

--- app/lib/route.ts
+++ app/lib/route.ts
@@ -2,12 +2,21 @@
 import {CatType} from "@/app/types/cat";
 
 export async function getCatData(): Promise<CatType | null> {
-    try{
-        const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10?api_key=${process.env.CAT_API_KEY}`);
+    // try{
+    //     const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10?api_key=${process.env.CAT_API_KEY}`);
+    //     const data = await res.json();
+    //     return data;
+    // } catch (error) {
+    //     console.error(error);
+    //     return null;
+    // }
+    const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10?api_key=${process.env.CAT_API_KEY}`);
+    if(res.status !== 200){
+        return [];
+    }else{
         const data = await res.json();
+        console.log(data);
         return data;
-    } catch (error) {
-        console.error(error);
-        return null;
     }
+
 }
